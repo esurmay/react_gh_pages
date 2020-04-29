f8ac39ac1b7908ae6342c39afcbb3c62ecff4ad0
Roger E Surmay <esurmay@gmail.com>
2020-04-29 10:06:25 +0200

    Updates

---
+++ precache-manifest.69f4cdbdd5c6b906a812b4e270badd92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441588bbfb309cf091249224225bfb88",
+    "url": "/react_gh_pages/index.html"
+  },
+  {
+    "revision": "f65546c5cb20fce447a6",
+    "url": "/react_gh_pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6cde97d3e9a79285c43",
+    "url": "/react_gh_pages/static/js/2.c6776c67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_gh_pages/static/js/2.c6776c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65546c5cb20fce447a6",
+    "url": "/react_gh_pages/static/js/main.2d686e61.chunk.js"
+  },
+  {
+    "revision": "1ea656ee75f7307a8657",
+    "url": "/react_gh_pages/static/js/runtime-main.4f35a980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_gh_pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
